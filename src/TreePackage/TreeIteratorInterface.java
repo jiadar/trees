5f6a00ded3827bdfc85db62abceb82c06388d713
Ross Capdeville <ross@teamengine.org>
2017-04-25 17:05:23 -0700

    initial commit

---
+++ src/TreePackage/TreeIteratorInterface.java
@@ -0,0 +1,16 @@
+// 25.17
+package TreePackage;
+import java.util.Iterator;
+/**
+ * An interface for iterators that perform traversals of a tree.
+ * 
+ * @author Frank M. Carrano
+ * @version 2.0
+ */
+public interface TreeIteratorInterface<T>
+{
+  public Iterator<T> getPreorderIterator();
+  public Iterator<T> getPostorderIterator();
+  public Iterator<T> getInorderIterator();
+  public Iterator<T> getLevelOrderIterator();
+} // end TreeIteratorInterface
